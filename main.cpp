27e9c15480b955e43e8b93e9586d7b6ddcb6818d
EthanThomson-ltb <ltbluet@gmail.com>
2026-02-23 09:13:23 -0700

    Part one finished

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+//Swaps the values of two variables, a and b
+void aBswap(int& a, int& b);
+
 int main() {
-    cout << "Initial Commit" << std;
+
+    cout << "---Part one---" << endl;
+    //Variable swap
+    int a, b;
+    cout << "input two numbers:" << endl;
+    cin >> a >> b;
+    cout << "The values of numbers a and b are " << a << " and " << b << endl;
+    aBswap(a, b);
+    cout << "The new swapped values of numbers a and b are " << a << " and " << b << endl;
+
     return 0;
+}
+
+void aBswap(int& a, int& b) {
+    int x = a;
+    a = b;
+    b = x;
 }
